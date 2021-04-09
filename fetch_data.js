5eff6f7a67f1fb81c3e59a30d81bc61cbc8b459c
smnshuvo <smnshuvo@gmail.com>
2021-04-09 17:38:57 +0600

    Ready for fetching data

--- fetch_data.js
+++ fetch_data.js
@@ -1,5 +1,7 @@
 // using the js instead to collect data
 let videos = document.querySelectorAll(".style-scope ytd-video-renderer");
 videos.forEach(function (video){
-
+    let videoTitle = video.querySelector('yt-formatted-string').innerText;
+    videoTitle.replace(",","|");
+    console.log(`"${videoTitle}",`);
 })
