ec2f5cc1d93d60ab696c17ccad0884f3869b4a91
smnshuvo <smnshuvo@gmail.com>
2021-04-09 17:46:50 +0600

    data collection round 3

--- fetch_data.js
+++ fetch_data.js
@@ -1,7 +1,10 @@
 // using the js instead to collect data
 let videos = document.querySelectorAll(".style-scope ytd-video-renderer");
+let finalData = ""
 videos.forEach(function (video){
     let videoTitle = video.querySelector('yt-formatted-string').innerText;
-    videoTitle.replace(",","|");
-    console.log(`"${videoTitle}",`);
+    videoTitle = videoTitle.replace(",","|");
+    finalData+= `"${videoTitle}",`;
+
 })
+console.log(finalData);
